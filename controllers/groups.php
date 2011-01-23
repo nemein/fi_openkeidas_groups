88e12d7d04d3fb76d0983aba8dec427dac2541ae
Henri Bergius <henri.bergius@iki.fi>
2011-01-23 19:59:41 +0200

    List user's groups

--- controllers/groups.php
+++ controllers/groups.php
@@ -21,6 +21,27 @@ class fi_openkeidas_groups_controllers_groups
         }
     }
 
+    public function get_user(array $args)
+    {
+        $member_qb = new midgard_query_builder('fi_openkeidas_groups_group_member');
+        $member_qb->add_constraint('person', '=', midgardmvc_core::get_instance()->authentication->get_person()->id);
+        $members = $member_qb->execute();
+        $group_ids = array();
+        foreach ($members as $member)
+        {
+            $group_ids[] = $member->grp;
+        }
+        $qb = new midgard_query_builder('fi_openkeidas_groups_group');
+        $qb->add_constraint('id', 'IN', $group_ids);
+        $qb->add_order('metadata.score', 'DESC');
+        $this->data['groups'] = array();
+        $groups = $qb->execute();
+        foreach ($groups as $group)
+        {
+            $this->data['groups'][] = $this->prepare_for_list($group);
+        }
+    }
+
     public function get_active(array $args)
     {
         $qb = new midgard_query_builder('fi_openkeidas_groups_group');
